3e2387fa41416972c8b6007cdbfb87e2781b2133
Athos-0day <athos.0day@protonmail.com>
2026-03-31 17:04:25 +0200

    chore: quelques corrections

--- src/main/java/com/projet/poker/model/persist/Amitie.java
+++ src/main/java/com/projet/poker/model/persist/Amitie.java
@@ -1,39 +1,37 @@
 package com.projet.poker.model.persist;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
-
 
 @Entity
 @Table(uniqueConstraints = {
-    @UniqueConstraint(columnNames = {"user1_id", "user2_id"})
-}) // evite une amitie AB et BA
+    @UniqueConstraint(columnNames = {"user_demandeur", "user_recepteur"})
+}, name = "amities") // evite une amitie AB et BA
 public class Amitie {
     // Attributs de l'entité Profil
 
+    // L'identifiant unique de l'amitie
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    // L'identifiant unique de l'amitie
     private Long id;
+
     // Status de l'amitie : PENDING, ACCEPTED, BLOCKED
+    @Column(nullable = false)
     private FriendStatus status;
+
     // Date de creation de l'amitie
+    @Column(nullable = false, updatable = false)
     private LocalDateTime since;
 
     // Relations avec d'autres entités
-    // Relation ManyToOne avec l'entité Utilisateur 1
+    // Relation ManyToOne avec l'entité Utilisateur demandeuse
     @ManyToOne
-    @JoinColumn(name = "user1_id")
-    private Utilisateur user1;
-    // Relation ManyToOne avec l'entité Utilisateur 2
+    @JoinColumn(name = "user_demandeur")
+    private Utilisateur user_demandeur;
+    // Relation ManyToOne avec l'entité Utilisateur receptrice 
     @ManyToOne
-    @JoinColumn(name = "user2_id")
-    private Utilisateur user2;
-
+    @JoinColumn(name = "user_recepteur")
+    private Utilisateur user_recepteur;
 
     // Constructeurs
     /**
@@ -47,16 +45,16 @@ public class Amitie {
     /**
      * Constructeur de l'entité Profil avec tous les attributs
      *
-     * @param user1 L'utilisateur 1 formant l'amitié
-     * @param user2 L'utilisateur 2 formant l'amitié
+     * @param user_demandeur L'utilisateur 1 formant l'amitié
+     * @param user_recepteur L'utilisateur 2 formant l'amitié
      * @param status L'etat de l'amitié : PENDING, ACCEPTED, BLOCKED
      * @param since La date de création de l'amitié
      */
-    public Amitie(Utilisateur user1, Utilisateur user2, FriendStatus status, LocalDateTime since) {
+    public Amitie(Utilisateur user_demandeur, Utilisateur user_recepteur, FriendStatus status, LocalDateTime since) {
         this.status = status;
         this.since = since;
-        this.user1 = user1;
-        this.user2 = user2;
+        this.user_demandeur = user_demandeur;
+        this.user_recepteur = user_recepteur;
         validerNonAutoAmitie();
     }
 
@@ -64,7 +62,7 @@ public class Amitie {
     @PrePersist
     @PreUpdate
     private void validerNonAutoAmitie() {
-        if (user1 != null && user2 != null && user1.getId().equals(user2.getId())) {
+        if (user_demandeur != null && user_recepteur != null && user_demandeur.getId().equals(user_recepteur.getId())) {
             throw new IllegalStateException("Un utilisateur ne peut pas créer une amitié avec lui-même.");
         }
     }
@@ -79,7 +77,7 @@ public class Amitie {
 
     /**
      * Setter pour le status de l'amitie
-     * 
+     *
      * @param status Nouveau status de l'amitie
      */
     public void setStatus(FriendStatus status) {
@@ -95,7 +93,7 @@ public class Amitie {
 
     /**
      * Getter pour la date de creation de l'amitie
-     * 
+     *
      * @param since Nouvelle date de creation de l'amitie
      */
     public void getDateCreation(LocalDateTime since) {
@@ -122,19 +120,17 @@ public class Amitie {
 
     // Getters et setters pour les relations avec d'autres entités
     /**
-     * Obtient l'utilisateur 1 associé à cette amitie
+     * Obtient l'utilisateur demandeur associé à cette amitie
      */
-    public Utilisateur getUser1() {
-        return user1;
+    public Utilisateur getUserDemandeur() {
+        return user_demandeur;
     }
 
     /**
-     * Obtient l'utilisateur 2 associé à cette amitie
+     * Obtient l'utilisateur recepteur associé à cette amitie
      */
-    public Utilisateur getUser2() {
-        return user2;
+    public Utilisateur getUserRecepteur() {
+        return user_recepteur;
     }
-
-
 
 }
