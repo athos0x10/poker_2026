4d6385e569c0dd23ee066adda90326823f6c8979
Louan <nop>
2026-04-01 14:18:17 +0200

    correction des erreur jakata

--- poker/src/main/java/com/projet/poker/repository/AmitieRepository.java
+++ poker/src/main/java/com/projet/poker/repository/AmitieRepository.java
@@ -1,9 +1,12 @@
 package com.projet.poker.repository;
 
-import com.projet.poker.model.persist.Amitie;
-import com.projet.poker.model.persist.Utilisateur;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import com.projet.poker.model.persist.Amitie;
+import com.projet.poker.model.persist.Utilisateur;
 
 @Repository
 public interface AmitieRepository extends JpaRepository<Amitie, Long> {
